1b544a8ccbb80d6431a626e1cd4f4f82f642d5a4
z4nder <gusttavo212@gmail.com>
2023-05-08 20:30:36 -0300

    Add progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,37 @@
 mod errors;
 mod file_manager;
 mod pest;
+use clap::Parser;
 
 use crate::{file_manager::insert_erorrs, pest::run_tests};
 
+#[derive(Parser)]
+struct Cli {
+    repeat: u64,
+    path: String,
+}
+
 fn main() {
-    let path = String::from("/home/zander/projects/personal/example-app");
+    let args = Cli::parse();
     let command = String::from(r#"./vendor/bin/pest"#);
     let file = String::from("output.json");
-    let repeat = 1;
 
-    for _i in 0..repeat {
+    let pb = indicatif::ProgressBar::new(args.repeat);
+
+    for _i in 0..args.repeat {
         println!("Run tests...");
 
-        let error_to_insert = run_tests(&file, &path, &command);
+        let error_to_insert = run_tests(&file, &args.path, &command);
 
         match error_to_insert {
             Ok(values) => {
                 insert_erorrs(values, &file);
-                println!("Finish, errors inserted...")
             }
             Err(error) => println!("{}", error.to_message()),
         }
+
+        pb.inc(1);
     }
+
+    pb.finish_with_message("done");
 }
